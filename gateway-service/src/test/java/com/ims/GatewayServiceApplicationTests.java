2f59c1f8962c4c64bb0d4f7fb87ed916228803f4
Rishika Jat <163402758+Rishika-jat@users.noreply.github.com>
2025-03-21 10:35:37 +0530

    Update GatewayServiceApplicationTests.java

--- gateway-service/src/test/java/com/ims/GatewayServiceApplicationTests.java
+++ gateway-service/src/test/java/com/ims/GatewayServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.ims;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = {TestConfig.class})
 @ContextConfiguration(classes = GatewayServiceApplication.class)
 class GatewayServiceApplicationTests {
 
