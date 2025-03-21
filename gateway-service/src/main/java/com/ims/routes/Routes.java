8d2e2fccb578d66a027088bc44b33f6f5bbd53b5
deepak.kumar <deepaksingh8080@gmail.com>
2025-03-21 14:55:40 +0530

    fix:jdbc-coonection

--- gateway-service/src/main/java/com/ims/routes/Routes.java
+++ gateway-service/src/main/java/com/ims/routes/Routes.java
@@ -12,20 +12,21 @@ import org.springframework.beans.factory.annotation.Value;
 @Configuration
 public class Routes {
 
-    
+
     @Value("${spring.product.port}")
     private String productPort;
 
-    
+
     @Value("${spring.invoice.port}")
     private String invoicePort;
-    
+
 
     @Bean
     public RouterFunction<ServerResponse> productServiceRoute(){
         System.out.println("in product route");
         return GatewayRouterFunctions.route("product_service")
-                .route(RequestPredicates.path("/api/product"), HandlerFunctions.http("http://product-service:"+productPort))
+                .route(RequestPredicates.path("/api/product"), HandlerFunctions.http("http://product-service:" + productPort))
+                .route(RequestPredicates.path("/api/product/{id}"), HandlerFunctions.http("http://product-service:" + productPort))
                 .build();
     }
 
@@ -33,7 +34,9 @@ public class Routes {
     public RouterFunction<ServerResponse> invoiceServiceRoute(){
         System.out.println("in service route");
         return GatewayRouterFunctions.route("invoice_service")
-                .route(RequestPredicates.path("/api/invoices"), HandlerFunctions.http("http://invoice-service:"+invoicePort))
+                .route(RequestPredicates.path("/api/invoices"), HandlerFunctions.http("http://invoice-service:" + invoicePort))
+                .route(RequestPredicates.path("/api/invoices/{id}"), HandlerFunctions.http("http://invoice-service:" + invoicePort))
+                .route(RequestPredicates.path("/api/invoices/items/{itemId}"), HandlerFunctions.http("http://invoice-service:" + invoicePort))
                 .build();
     }
 }
