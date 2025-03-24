6c490970aa1536dbf3c4af88c24ab6f1f06236a8
Rishika Jat <163402758+Rishika-jat@users.noreply.github.com>
2025-03-24 21:13:01 +0530

    Update ProductController.java

--- product-service/src/main/java/com/ims/controller/ProductController.java
+++ product-service/src/main/java/com/ims/controller/ProductController.java
@@ -11,9 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/product")
-@CrossOrigin(origins = "*")
-
-//@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://localhost:5173")
 @RequiredArgsConstructor
 public class ProductController {
 
