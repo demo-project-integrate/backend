07edc7282d34238270db8d96c5bc526fd6f966b8
Rishika Jat <163402758+Rishika-jat@users.noreply.github.com>
2025-03-24 15:52:28 +0530

    Update SecurityConfig.java

--- gateway-service/src/main/java/com/ims/config/SecurityConfig.java
+++ gateway-service/src/main/java/com/ims/config/SecurityConfig.java
@@ -15,7 +15,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/api/product", "/api/product/**").permitAll()
+                        .requestMatchers(request -> request.getHeader("Authorization") == null).permitAll() 
                         .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
